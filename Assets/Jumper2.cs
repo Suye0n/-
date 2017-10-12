e0ff04c334ee4d37be3a5c443fdcae2dc5680009
Suye0n <incuriositas@naver.com>
2017-10-12 18:51:05 +0900

    유니티s

---
+++ Assets/Jumper2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper2 : MonoBehaviour {
+
+    GameObject jumper3;
+    // Use this for initialization
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Vector3 direction = jumper3.transform.position - transform.position;
+        direction = direction.normalized * 1000;
+        collision.gameObject.GetComponent<Rigidbody>().AddForce(direction);
+    }
+    void Start () {
+        jumper3 = GameObject.Find("jumper3");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
